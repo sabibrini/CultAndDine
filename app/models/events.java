23381702ea9ffb54386b632d06fe696d900ed77d
lena-raedler <anna-lena.raedler@student.uibk.ac.at>
2015-11-17 15:59:01 +0100

    initial commit

---
+++ app/models/events.java
@@ -0,0 +1,48 @@
+ package models;
+
+
+public class events{
+    private String Title;
+    private String link;
+    private String Description;
+    private String Category;
+    
+    public events(String Title,String link,String Description,String Category){
+        this.Title=Title;
+        this.link=link;
+        this.Description=Description;
+        this.Category=Category;
+    }
+     public String getTitle() {
+		return Title;
+	}
+
+	public void setTitle(String title) {
+		Title = title;
+	}
+
+	public String getLink() {
+		return link;
+	}
+
+	public void setLink(String link) {
+		this.link = link;
+	}
+
+	public String getDescription() {
+		return Description;
+	}
+
+	public void setDescription(String description) {
+		Description = description;
+	}
+
+	public String getCategory() {
+		return Category;
+	}
+
+	public void setCategory(String category) {
+		Category = category;
+	}
+
+}
---
+++ app/models/read_xml.java
@@ -0,0 +1,49 @@
+package models;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+import java.io.File;
+import java.util.ArrayList;
+
+public class read_xml {
+    
+    public static ArrayList<events> event=new ArrayList<events>();
+
+    public static void read(){
+
+    try {
+
+	File fXmlFile = new File("/home/ramona/Dokumente/playapp/cultDine/events.xml");
+	DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+	DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+	Document doc = dBuilder.parse(fXmlFile);
+			
+	
+	doc.getDocumentElement().normalize();
+
+			
+	NodeList nList = doc.getElementsByTagName("item");
+			
+	System.out.println("----------------------------");
+
+	for (int temp = 0; temp < nList.getLength(); temp++) {
+
+		Node nNode = nList.item(temp);
+				
+		System.out.println("\nCurrent Element :" + nNode.getNodeName());
+				
+		if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+			Element eElement = (Element) nNode;
+                        event.add( new events(eElement.getElementsByTagName("title").item(0).getTextContent(),eElement.getElementsByTagName("link").item(0).getTextContent(),eElement.getElementsByTagName("description").item(0).getTextContent(), eElement.getElementsByTagName("category").item(0).getTextContent()));
+		}
+	}
+    } catch (Exception e) {
+	e.printStackTrace();
+    }
+  }
+  }
+
