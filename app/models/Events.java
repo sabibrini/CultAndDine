5e20e087278f9fae3f232155e1a21c0a7ee41be8
sabibrini <sabrina.schmitzer@gmx.at>
2015-12-01 15:49:56 +0100

    changed events site

--- app/models/Events.java
+++ app/models/Events.java
@@ -3,15 +3,15 @@
 
 public class Events{
     private String Title;
-    private String link;
-    private String Description;
+   // private String Description;
     private String Category;
+    private String Organizer;
     
-    public Events(String Title,String link,String Description,String Category){
+    public Events(String Title, String Category,/* String Description,*/ String Organizer){
         this.Title=Title;
-        this.link=link;
-        this.Description=Description;
+       // this.Description=Description;
         this.Category=Category;
+        this.Organizer = Organizer;
     }
      public String getTitle() {
 		return Title;
@@ -21,21 +21,14 @@ public class Events{
 		Title = title;
 	}
 
-	public String getLink() {
-		return link;
-	}
 
-	public void setLink(String link) {
-		this.link = link;
-	}
-
-	public String getDescription() {
-		return Description;
-	}
-
-	public void setDescription(String description) {
-		Description = description;
-	}
+//	public String getDescription() {
+//		return Description;
+//	}
+//
+//	public void setDescription(String description) {
+//		Description = description;
+//	}
 
 	public String getCategory() {
 		return Category;
@@ -43,6 +36,14 @@ public class Events{
 
 	public void setCategory(String category) {
 		Category = category;
+	}
+	
+	public String getOrganizer() {
+		return Organizer;
+	}
+	
+	public void setOrganizer(String organizer) {
+		Organizer = organizer;
 	}
 
 }
